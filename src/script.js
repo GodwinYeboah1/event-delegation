e62f38b028500690ea069db82b2b06bdc2da643b
godwinyeboah1 <godwin.y15@gmail.com>
2025-01-22 13:32:38 -0500

    Clean up js

--- src/script.js
+++ src/script.js
@@ -5,18 +5,18 @@ const boxs = document.querySelectorAll('.box');
 
 addButton.addEventListener('click', function() {
     const box = document.createElement('div');
-    box.setAttribute("class", "box");
-    grid.appendChild(box);
+    box.classList.add("box");
+    grid.append(box);
 });
 
 function addGlobalEventListner(type, selector, callback, parent = document) {
-    parent.addEventListener(type, function(e) {
+    parent.addEventListener(type, (e)=>  {
         if(e.target.matches(selector)) {
             callback(e);
         }
     });
 }
 
-addGlobalEventListner('click', '.box',(e)=>{
+addGlobalEventListner('click', '.box',(e)=> {
     e.target.classList.toggle('clicked');
 }, grid);
